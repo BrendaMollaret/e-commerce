e8226a9903b3b79db1e302ef248cde59ccdd2212
Brenda Mollaret <mollaretbrenda@gmail.com>
2023-07-03 20:07:34 -0300

    Creación de la estructura del proyecto y las clases model

---
+++ src/test/java/com/proyecto/eCommerce/ListaDeTareas1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyecto.eCommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListaDeTareas1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
